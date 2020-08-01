54ecb39a2c37f19d67433ea05be74b71260033a2
Nebil Muren <46371510+nmuren@users.noreply.github.com>
2020-08-02 02:57:14 +0300

    route comments added

--- src/product/product.route.js
+++ src/product/product.route.js
@@ -3,8 +3,12 @@ module.exports = function (app) {
 
   const ProductController = require("../product/product.controller");
 
+  //Return all the products
   app.get(`${urlPrefix}`, ProductController.getAllForDatatable);
+  //Create new product
   app.post(`${urlPrefix}/add`, ProductController.create);
+  //Update given product's data
   app.put(`${urlPrefix}/update`, ProductController.update);
+  //Delete the product/s with given id/s
   app.delete(`${urlPrefix}/delete`, ProductController.delete);
 };
